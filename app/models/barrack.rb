619ca316191ff2fc39fa43b768eb59a2c253d920
Naki21 <Yianni21@gmail.com>
2016-12-01 11:51:55 -0500

    Allows for Barracks creation with initialized variables

--- app/models/barrack.rb
+++ app/models/barrack.rb
@@ -1,4 +1,12 @@
+# frozen_string_literal: true
+include Giphy
 class Barrack < ActiveRecord::Base
   belongs_to :user, inverse_of: :barracks
   belongs_to :fighter, inverse_of: :barracks
+  def initialize(barrack_params)
+    super
+    self.opponent = Giphy.random.id
+    self.u_val = rand(100)
+    self.f_val = rand(100)
+  end
 end
--- app/serializers/barrack_serializer.rb
+++ app/serializers/barrack_serializer.rb
@@ -1,5 +1,5 @@
 class BarrackSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :u_val, :f_val, :win?, :opponent
   has_one :user
   has_one :fighter
 end
