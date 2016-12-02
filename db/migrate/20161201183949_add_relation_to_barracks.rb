77e2fab99889ffb8d43ef9888e81f7ede924d22a
Naki21 <Yianni21@gmail.com>
2016-12-02 17:42:00 -0500

    Edits relations for figher table

--- db/migrate/20161201183949_add_relation_to_barracks.rb
+++ db/migrate/20161201183949_add_relation_to_barracks.rb
@@ -2,6 +2,5 @@
 class AddRelationToBarracks < ActiveRecord::Migration
   def change
     add_reference :barracks, :battle, index: true, foreign_key: true
-    remove_reference :barracks, :fighter, index: true, foreign_key: true
   end
 end
