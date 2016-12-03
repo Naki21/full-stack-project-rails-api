7fa7382d3cfa385c31a681e7f540ecd814d8762d
Naki21 <Yianni21@gmail.com>
2016-12-02 22:29:25 -0500

    adds update curreny fighter finctionality

--- scripts/change-password.sh
+++ scripts/change-password.sh
@@ -9,3 +9,17 @@ curl --include --request PATCH http://localhost:4741/change-password/$ID \
       "new": "super sekrit"
     }
   }'
+
+  curl --include --request POST http://localhost:4741/fighters/4 \
+    --data '{
+      "fighter": {
+        "url": "an example password",
+    }'
+    curl --include --request PUT http://localhost:4741/barracks/6 \
+      --data '{
+"barrack": {
+"id": 6,
+"user_id": 1,
+"url": null
+}
+}'
