bb0886c0e05e08d95abf878705782f6b8a0b7b35
Naki21 <Yianni21@gmail.com>
2016-12-14 18:23:56 -0500

    giphy env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,4 +60,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  Giphy::Configuration.configure do |config|
+    config.version = ENV['THE_API_VERSION']
+    config.api_key = ENV['DEFAULT_API_KEY']
+  end
 end
